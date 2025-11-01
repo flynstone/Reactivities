7fbc250a72666f23e3b07307d6d5f5583cad156f
Julien <flynstone@live.ca>
2025-11-01 15:00:01 -0400

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -4,16 +4,19 @@ using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000", "http://127.0.0.1:3000", "https:127.0.0.1:3000"));
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
