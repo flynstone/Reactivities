638d4621e8dd9797e8022d070126b2e9ec7d97fd
Julien <flynstone@live.ca>
2026-03-15 14:01:06 -0400

    End of section 6

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,13 +1,15 @@
 import { Box, Button, Card, CardActions, CardContent, Chip, Typography } from "@mui/material";
+import { useActivities } from "../../../lib/hooks/useActivities";
 
 type Props = {
     activity: Activity;
     selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityCard({activity, selectActivity, deleteActivity}: Props) {
-  return (
+export default function ActivityCard({activity, selectActivity}: Props) {
+  const { deleteActivity } = useActivities();
+
+    return (
     <Card>
         <CardContent>
             <Typography variant='h5'>{activity.title}</Typography>
@@ -20,8 +22,12 @@ export default function ActivityCard({activity, selectActivity, deleteActivity}:
             <Box display="flex" gap={3}>
                   <Button onClick={() => selectActivity(activity.id)} size='medium'
                       variant='contained'>View</Button>   
-                  <Button onClick={() => deleteActivity(activity.id)} size='medium'color='error'
-                      variant='contained'>Delete</Button>
+                    <Button
+                        onClick={() => deleteActivity.mutate(activity.id)}
+                        disabled={deleteActivity.isPending}
+                        size='medium'
+                        color='error'
+                        variant='contained'>Delete</Button>
             </Box>
               
         </CardActions>
